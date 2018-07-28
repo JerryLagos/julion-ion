964049771281f286eb8e97f7a79c1764cab0cf98
JerryLagos <jerrysebastianlagosc@gmail.com>
2018-07-28 10:09:47 -0500

    Para estudiantes de julio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,32 +3,45 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
+
+// Bienvenido sea mi listado de paginas.
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { CountPage } from '../pages/count/count';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { MyserviceProvider } from '../providers/myservice/myservice';
+import { HttpClientModule, HttpClient } from '../../node_modules/@angular/common/http';
+import { OtherServiceProvider } from '../providers/other-service/other-service';
+
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    CountPage
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    CountPage
   ],
   providers: [
     StatusBar,
+    HttpClient,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    MyserviceProvider,
+    OtherServiceProvider
   ]
 })
 export class AppModule {}
